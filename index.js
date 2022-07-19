c160e9d656b12768103c76edfcb10419c8739d26
Richard King <richrdkng@gmail.com>
2022-07-19 21:57:53 +0200

    feat: initial release

---
+++ index.js
@@ -0,0 +1,88 @@
+const AggregateError = require('aggregate-error')
+
+const _verifyConfig = require('./lib/verify-config')
+const _verifyAuth   = require('./lib/verify-auth')
+const _prepare      = require('./lib/prepare')
+const _publish      = require('./lib/publish')
+
+// -----------------------------------------------------------------------------
+
+let verified
+let prepared
+
+// -----------------------------------------------------------------------------
+
+async function verifyConditions(pluginConfig, context) {
+  const errors = _verifyConfig(pluginConfig, context)
+
+  try {
+    await _verifyAuth(pluginConfig, context)
+  } catch (error) {
+    errors.push(...error)
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors)
+  }
+
+  verified = true
+}
+
+// -----------------------------------------------------------------------------
+
+async function prepare(pluginConfig, context) {
+  const errors = !verified
+    ? _verifyConfig(pluginConfig, context)
+    : []
+
+  try {
+    if (!verified) {
+      await _verifyAuth(pluginConfig, context)
+    }
+  } catch (error) {
+    errors.push(...error)
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors)
+  }
+
+  await _prepare(pluginConfig, context)
+
+  prepared = true
+}
+
+// -----------------------------------------------------------------------------
+
+async function publish(pluginConfig, context) {
+  const errors = !verified
+    ? _verifyConfig(pluginConfig, context)
+    : []
+
+  try {
+    if (!verified) {
+      await _verifyAuth(pluginConfig, context)
+    }
+  } catch (error) {
+    errors.push(...error)
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors)
+  }
+
+  if (!prepared) {
+    await _prepare(pluginConfig, context)
+    prepared = true
+  }
+
+  return _publish(pluginConfig, context)
+}
+
+// -----------------------------------------------------------------------------
+
+module.exports = {
+  verifyConditions,
+  prepare,
+  publish,
+}
