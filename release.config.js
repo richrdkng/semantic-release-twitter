8827ee7b44c27ffa7042bf9370f950bf89f9b8e1
Richard King <richrdkng@gmail.com>
2022-07-21 10:34:10 +0200

    ci: simplify release config

--- release.config.js
+++ release.config.js
@@ -11,9 +11,7 @@ module.exports = {
       pkgRoot: '.dist',
     }],
     ['@semantic-release/git', {
-      assets: [
-        'CHANGELOG*',
-      ],
+      assets: 'CHANGELOG*',
     }],
   ],
 }
